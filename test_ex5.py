0a387a6a531ebb5ee5178e551c2866571174a743
brunothetav <brunoccalmeida@hotmail.com>
2021-11-13 16:48:33 -0600

    Exercício 5 e seus testes terminados

--- test_ex5.py
+++ test_ex5.py
@@ -1,1 +1,29 @@
 from Ex5 import ContaCorrente
+from unittest import TestCase
+
+
+class TestContaCorrente(TestCase):
+
+    def teste_alterar_nome(self):
+        correntista1 = ContaCorrente(23044, 'Bruno', 54000)
+        self.assertEqual('Bruno', correntista1.nome_correntista)
+        correntista1.alterar_nome('Bruno Almeida')
+        self.assertEqual('Bruno Almeida', correntista1.nome_correntista)
+
+    def teste_deposito(self):
+        correntista1 = ContaCorrente(23044, 'Bruno', 54000)
+        self.assertEqual(54000, correntista1.saldo)
+        correntista1.deposito(330)
+        self.assertEqual(54330, correntista1.saldo)
+
+    def teste_saque(self):
+        correntista1 = ContaCorrente(23044, 'Bruno', 54000)
+        correntista1.saque(53000)
+        self.assertEqual(1000, correntista1.saldo)
+        self.assertEqual('Saldo insuficiente.', correntista1.saque(2000))
+        self.assertEqual(1000, correntista1.saldo)
+
+        correntista2 = ContaCorrente(23034, 'Gina')
+        self.assertEqual(0, correntista2.saldo)
+        self.assertEqual('Saldo insuficiente.', correntista2.saque(2000))
+        self.assertEqual(0, correntista2.saldo)
